c0b7d77ed69ab539c88809e939bcd1ced4325164
Shadoweagle7 <38882778+Shadoweagle7@users.noreply.github.com>
2023-12-18 19:35:08 +1100

    Restarting from scratch lol

--- SE7 Compiler/SE7C/SE7C/SE7C.cpp
+++ SE7 Compiler/SE7C/SE7C/SE7C.cpp
@@ -1,33 +1,9 @@
 // SE7C.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <iostream>
-#include <fstream>
-#include <vector>
-#include <string>
-#include <filesystem>
 
-#include "lexer.hpp"
 
 int main(int argc, const char *argv[]) {
-	if (!std::filesystem::exists("test.SE7")) {
-		std::fstream temp("test.SE7", std::ios_base::in | std::ios_base::out | std::ios_base::trunc);
-
-		temp <<
-			"module src;\n"
-			"\n"
-			"import io;\n"
-			"\n"
-			"int(string[]) main = (args) {\n"
-			"\r\tint a = 1 + 2 * 3 / 4 \\ 5 & 6 | 7 ^ 8 - 9;\n"
-			"\r\t\n"
-			"\r\tprintln(args[0]);\n"
-			"\r\treturn 0;\n"
-			"}\n"
-		;
-
-		temp.close();
-	}
 
 	return 0;
 }
